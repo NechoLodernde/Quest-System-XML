7b70c9f2f37ba61777d0cad753b9e30587b7e4b6
Jovian Kurnia <jovian1gdf@gmail.com>
2023-11-01 16:19:13 +0700

    Update-1/11/2023-Part.01
    - Added logic for third person camera control and movement

---
+++ Assets/Scripts/Player_Essentials/ThirdPersonMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y);
+
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
