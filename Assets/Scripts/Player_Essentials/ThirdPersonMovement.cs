51fcf9609a1a1e138f1926067a9a8a38ae5e4314
Jovian Kurnia <jovian1gdf@gmail.com>
2023-11-02 16:43:31 +0700

    Update-02/11/2023-Part.01
    - Finishes player movement script and camera control
    - Switches from old input system to new input system
    - Modifying environment aspect and project script

--- Assets/Scripts/Player_Essentials/ThirdPersonMovement.cs
+++ Assets/Scripts/Player_Essentials/ThirdPersonMovement.cs
@@ -1,26 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+    public PlayerActions controls;
 
     public float speed = 6f;
 
-    // Update is called once per frame
-    void Update()
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Awake()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        controls = new PlayerActions();
+        //controls.PlayerMain.Move.performed += _ => WeMove();
+        //controls.PlayerMain.Look.performed += _ => WeLook();
+    }
+
+    void WeMove()
+    {
+        //Debug.Log("We move the player");
+
+        Vector2 movementInput = controls.PlayerMain.Move.ReadValue<Vector2>();
+        float horizontal = movementInput.x;
+        float vertical = movementInput.y;
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+    void WeLook()
+    {
+        //Debug.Log("We change the look direction");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
+        WeMove();
     }
 }
