6b74925f87c8af183706497b0f2479675a818cb6
Jovian Kurnia <jovian1gdf@gmail.com>
2023-11-09 16:45:24 +0700

    Update-09/11/2023-Part.01
    - Update Third person movement function to optimize flow

--- Assets/Scripts/Player_Essentials/ThirdPersonMovement.cs
+++ Assets/Scripts/Player_Essentials/ThirdPersonMovement.cs
@@ -43,7 +43,7 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(speed * Time.deltaTime * moveDir.normalized);
         }
     }
 
@@ -63,7 +63,7 @@ public class ThirdPersonMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
         //Keyboard kb = InputSystem.GetDevice<Keyboard>();
         //if (kb.spaceKey.wasPressedThisFrame)
